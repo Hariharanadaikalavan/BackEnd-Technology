0119624da8fd1e63c3639a10adc8585d7146f17f
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-13 23:45:21 +0530

    Datatypes.java

---
+++ Java/Datatypes.java
@@ -0,0 +1,23 @@
+class Datatypes
+{
+public static void main(String[]args)
+{
+boolean bol = true;
+char charcter = '$';
+byte small = 23;
+short mini=555;
+int number=123456;
+long mobileNumber=8944707484;
+float decimal=23.00;
+double longDecimal=23.23333;
+System.out.println(bol);
+System.out.println(charcter);
+System.out.println(small);
+System.out.println(mini);
+System.out.println("int :"+number);
+System.out.println(mobileNumber);
+System.out.println(decimal);
+System.out.println(longDecimal);
+System.out.println("Non Primitive DataTypes");
+}
+}

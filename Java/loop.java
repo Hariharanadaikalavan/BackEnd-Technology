5778b34052b4c92bf36cbf22cf3cc250f698e209
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-11-13 23:42:35 +0530

    loop.java

---
+++ Java/loop.java
@@ -0,0 +1,12 @@
+class loop{
+  public static void main(String args[])
+  {
+    int a=20;
+    int b=20;
+
+    for(int i=0;i<b;i++)
+      {
+        a++;
+      }
+    System.out.println(a);
+    

dcdb8230590587b9bcd5abf20c1454cee94f57e2
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-27 23:38:02 +0530

    Update and rename 30.uniqueElement.java to 30.FindKeyElement.java

---
+++ Java/Day-3/30.FindKeyElement.java
@@ -0,0 +1,14 @@
+class Dummy {
+    public static void main(String args[]) {
+        int arr[] = {11, 22, 33, 44, 55, 66};
+        int key=66;
+      for(int i=0;i<arr.length;i++){
+          if(arr[i]==key){
+              System.out.print("key Found "+ arr[i]+"  @ "+i+"th index");
+          }
+      }
+     
+ 
+       
+    }
+}

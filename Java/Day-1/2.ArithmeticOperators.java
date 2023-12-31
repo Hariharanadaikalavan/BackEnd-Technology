03edfa381bf57ed48fe56262bc539697b4d6e574
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-31 23:53:57 +0530

    Update 2.ArithmeticOperators.java

--- Java/Day-1/2.ArithmeticOperators.java
+++ Java/Day-1/2.ArithmeticOperators.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.Scanner; // By using Arithmetic operations 
 public class ArithmeticOperators_2 {
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);

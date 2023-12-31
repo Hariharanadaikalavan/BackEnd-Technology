71a8c34b405286228f72657762e6fc2126345ad0
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2023-12-31 23:53:06 +0530

    Update 8.SystemTime.java

--- Java/Day-1/8.SystemTime.java
+++ Java/Day-1/8.SystemTime.java
@@ -1,4 +1,4 @@
-public class SystemTime_8 {
+public class SystemTime_8 {// Print current time 
     public static void main(String args[]){
         System.out.println(java.time.LocalDateTime.now());
     }

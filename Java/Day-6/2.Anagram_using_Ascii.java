7db67534fa74f9576fb05e32cb49b179421ae138
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2024-01-15 23:55:33 +0530

    Update 2.Anagram_using_Ascii.java

--- Java/Day-6/2.Anagram_using_Ascii.java
+++ Java/Day-6/2.Anagram_using_Ascii.java
@@ -1,4 +1,4 @@
-public class Anagram {                                     
+public class Anagram {                                       
     public static void main(String args[]){
          String str1="hariharan";
        String str2="harharian";

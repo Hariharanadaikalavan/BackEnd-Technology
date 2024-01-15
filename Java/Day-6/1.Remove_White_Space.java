5f8aaeb10a9a6fc03ecfae79521336362f9efbe0
Hariharan A <143572984+Hariharanadaikalavan@users.noreply.github.com>
2024-01-15 23:56:02 +0530

    Update 1.Remove_White_Space.java

--- Java/Day-6/1.Remove_White_Space.java
+++ Java/Day-6/1.Remove_White_Space.java
@@ -9,7 +9,7 @@ public class Remove_White_Space {
        for(int i=0;i<str.length();i++) // its prints only condition is statifying the condition is (chart at (index) is empty then its leave 
        {
        
-                    if(str.charAt(i)!=' ') 
+                    if(str.charAt(i)!=' ')   
                     {
                      str2=str2+str.charAt(i);
                     }
